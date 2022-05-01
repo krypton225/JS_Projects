00d49dfa32047c4f99b2687d2a8f2a65556ff3b8
krypton225 <k.mohamed.11.98@gmail.com>
2022-05-01 15:01:39 +0200

    edit @ index,main | add notification project

---
+++ js/notification.js
@@ -0,0 +1,90 @@
+let mySelectionIconNotification = document.querySelectorAll(
+  ".notification__list__selections .fa-ellipsis-h"
+);
+
+let notificationListSelectionsContent = document.querySelectorAll(
+  ".notification__list__selections__content"
+)[0];
+
+let notificationIconBell = document.getElementById("notificationIcon");
+
+/**
+ * * Select bell icon and show the body of the notification list.
+ **/
+
+notificationIconBell.addEventListener("click", () => {
+  let notificationList = document.querySelector(".notification__list");
+  notificationList.classList.toggle("noti-list-show");
+});
+
+/**
+ * * Select one ellipsis icon for each notification and get the content of the list selections.
+ **/
+
+mySelectionIconNotification.forEach((icon) => {
+  icon.addEventListener("click", (eo) => {
+    icon.classList.toggle("show-ellipsis-icon");
+
+    let getMyContent = eo.target.parentElement.getElementsByClassName(
+      "notification__list__selections__content"
+    )[0];
+
+    // console.log(getMyContent);
+    // console.log(getMyContent.nextElementSibling);
+
+    getMyContent.classList.toggle("visible-selection-content");
+  });
+});
+
+/**
+ * * Select one notification and get the content of it and delete from the list.
+ **/
+
+const deleteOneNotificcation = () => {
+  let containerOfDelete = document.querySelectorAll(
+    ".notification__list__selections__delete"
+  );
+
+  containerOfDelete.forEach((itemWillDelete) => {
+    itemWillDelete.addEventListener("click", (eo) => {
+      eo.preventDefault();
+
+      let myNotiInfo =
+        eo.target.parentElement.parentElement.parentElement.parentElement;
+
+      //   console.log(myNotiInfo);
+      myNotiInfo.style.display = "none";
+    });
+  });
+};
+
+deleteOneNotificcation();
+
+/**
+ * * Select one notification and mark as read.
+ **/
+
+const markAsReadNotification = () => {
+  let containerOfMarkRead = document.querySelectorAll(
+    ".notification__list__selections__read"
+  );
+
+  containerOfMarkRead.forEach((itemWillMarAsRead) => {
+    itemWillMarAsRead.addEventListener("click", (eo) => {
+      eo.preventDefault();
+
+      let getMyOneNotification =
+        eo.target.parentElement.parentElement.previousElementSibling.querySelectorAll(
+          "p"
+        );
+
+      //   console.log(getMyOneNotification);
+
+      getMyOneNotification.forEach((oneParagraph) => {
+        oneParagraph.classList.toggle("lighter-font");
+      });
+    });
+  });
+};
+
+markAsReadNotification();
