55171c49f7ee02242da858ae3fb362b7cf0f3ac5
krypton225 <k.mohamed.11.98@gmail.com>
2022-06-02 18:18:41 +0200

    EDIT in awesome_input.js

--- js/awesome_input.js
+++ js/awesome_input.js
@@ -1,20 +1,22 @@
-let mySpans = document.querySelectorAll("label span"),
-  allInputs = document.querySelectorAll("input");
-
-const movingCharacters = () => {
-  mySpans.forEach((el) => {});
-};
+let allInputs = document.querySelectorAll("input");
 
 allInputs.forEach((input) => {
   input.addEventListener("focus", (e) => {
     e.target.nextElementSibling.querySelectorAll("span").forEach((span) => {
-      span.classList.toggle("active");
+      span.classList.add("active");
     });
   });
 
   input.addEventListener("blur", (e) => {
-    e.target.nextElementSibling.querySelectorAll("span").forEach((span) => {
-      span.classList.remove("active");
-    });
+    let mySpans = e.target.nextElementSibling.querySelectorAll("span");
+    if (input.value.length === 0) {
+      mySpans.forEach((span) => {
+        span.classList.remove("active");
+      });
+    } else {
+      mySpans.forEach((span) => {
+        span.classList.add("active");
+      });
+    }
   });
 });
